b0d7da212b6e43c35f75db603e1e020cfeaa2ef6
Mukti <fzjlovemy@163.com>
2018-07-19 18:42:09 +0800

    df

--- src/page.js
+++ src/page.js
@@ -8,6 +8,7 @@ function Page(opt){
     this.el = opt.el || '#page'
     this.current = opt.current || 1
     this.total = opt.total || 1
+    this.showNum = opt.showNum || 5
     this.callback = opt.callback || function () {}
     if (opt.current > opt.total) {
         this.current = opt.total
@@ -21,24 +22,31 @@ Page.prototype.init = function () {
         return
     }
 
+    var numBefore = parseInt(this.showNum / 2)
+    var numAfter = parseInt(this.showNum / 2)
+    if (this.showNum % 2 === 0) {
+        numBefore--
+    }
+
     var h = []
-    // 第一个显示页码
-    var first = this.current - 3
     // 第一个页码必须大于 0
-    do{
-        first++
-    }while(first < 1)
+    if (this.current - numBefore < 1) {
+        numBefore = this.current - 1
+    }
+    if (this.current + numAfter > this.total) {
+        numAfter = this.total - this.current
+    }
     h.push('<ul class="page-content"><li><a class="page-prev" href="javascript:;"></a></li>')
-    if(this.current > 3){
+    if(this.current - numBefore > 1){
         // 就算当前页大于 3，第 1 页也是必然显示的
         h.push('<li><a href="javascript:;">1</a></li>')
-        if(this.current > 4){
+        if(this.current - numBefore > 2){
             // 当前页面大于 4，省略前面页码
             h.push('<li><span>...</span></li>')
         }
     }
-    console.log(first,this.current)
-    for(var i = first; i <= this.current + 2; i++){
+
+    for(var i = this.current - numBefore; i <= this.current + numAfter; i++){
         if(i <= this.total){
             if(i == this.current){
                 h.push('<li><a class="active" href="javascript:;">' + i + '</a>')
@@ -48,8 +56,8 @@ Page.prototype.init = function () {
             h.push('</li>')
         }
     }
-    if(this.total - this.current > 2){
-        if(this.total - this.current > 3){
+    if(this.current + numBefore < this.total - 1){
+        if(this.current + numBefore < this.total - 2){
             h.push('<li><span>...</span></li>')
         }
         h.push('<li><a href="javascript:;">' + this.total + '</a></li>')
@@ -57,6 +65,7 @@ Page.prototype.init = function () {
     h.push('<li><a class="page-next" href="javascript:void(0)"></a></li></ul>')
     $(this.el).html(h.join(''))
     var that = this
+
     $('ul.page-content li a').on('click', function(){
         if ($(this).hasClass('page-prev')) {
             that.current --
@@ -73,7 +82,7 @@ Page.prototype.init = function () {
                 that.current = that.total
             }
         } else {
-            that.current = $(this).html()
+            that.current = +$(this).html()
             if(that.callback){
                 that.callback(that.current)
             }
